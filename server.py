a93f84d79e3e2de3be3c375d695499400f6f04f4
Pratik <pratikshrivastav20@gmail.com>
2023-04-06 14:45:47 +0530

    REFRENCE

--- server.py
+++ server.py
@@ -143,14 +143,13 @@ async def read_index():
 async def UploadImage(mypic1:UploadFile=File(...), mypic2:UploadFile=File(...)):
     mypic1.filename = f"{uuid.uuid4()}.png"
     mypic2.filename = f"{uuid.uuid4()}.png"
+    
     contents1 = await mypic1.read()
     with open(f"{IMAGEDIR}{mypic1.filename}","wb") as f:
         f.write(contents1)
     contents2 = await mypic2.read()
     with open(f"{IMAGEDIR}{mypic2.filename}","wb") as f:
         f.write(contents2)
-    print(mypic1.filename)
-    print(mypic2.filename)
     image1_data = Image.open('imagesnew/' + mypic1.filename)
     image1_data = image1_data.convert("L")
     image2_data = Image.open('imagesnew/' + mypic2.filename)
@@ -162,15 +161,24 @@ async def UploadImage(mypic1:UploadFile=File(...), mypic2:UploadFile=File(...)):
     image1_data = image1_data.unsqueeze(0)
     image2_data = transform(image2_data)
     image2_data = image2_data.unsqueeze(0)
-    ans = net.forward_once(image1_data)
+    ans = net.forward(image1_data,image2_data)
     print(ans)
-    # print(ans[0][1])
-    res = ans.detach().numpy()
+    '''# print(ans[0][1])
+    #res = ans.detach().numpy()
+    #resFlat = res.flatten()
+    print(resFlat)
+    if resFlat[1] > 0.50:
+        return FileResponse('result.html')
+    else:
+        return FileResponse('forgedresult.html')'''
+
+    newans = ans[0]
+    res = newans.detach().numpy()
     resFlat = res.flatten()
     print(resFlat)
     if resFlat[1] > 0.50:
         return FileResponse('result.html')
     else:
         return FileResponse('forgedresult.html')
-
-    # return str(ans[0][1])
+    print(type(res))
+    print(newans)
